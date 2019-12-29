a0dcb03a625ea202fcd7d5affae43d5bbf2d008f
Ryan Braving <ryan.braving@gmail.com>
2019-12-29 21:23:38 +1300

    Added serializer for name field

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class HelloApiView(APIView):
-    """Test API View"""
+    """Test API Viewwe need """
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
