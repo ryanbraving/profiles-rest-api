56cc6fae368f3511f91d9c0dfb1e13211cc1e435
Ryan Braving <ryan.braving@gmail.com>
2020-01-01 21:48:57 +1300

    Created API ViewSet, Added list function, Registered router

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
 class HelloApiView(APIView):
-    """Test API Viewwe need """
+    """Test API View we need """
     serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
@@ -47,3 +48,17 @@ class HelloApiView(APIView):
         """Delete an object"""
 
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self, request):
+        """Return a hello message"""
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello', 'a_viewset': a_viewset})
