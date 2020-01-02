6bd6806c1fb5138094871cc39bb9fa2171d41597
Ryan Braving <ryan.braving@gmail.com>
2020-01-02 23:34:33 +1300

    default update function overwritten to use hash password

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -30,3 +30,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
         return user
 
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
