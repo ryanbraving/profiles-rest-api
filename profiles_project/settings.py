39c805931f0e70f79f56efa2bb61837ebe2a891f
Ryan Braving <ryan.braving@gmail.com>
2020-01-05 22:12:56 +1300

    Deployment configuration done

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lv5y%a8c34dvf_k2y&#khbfvrbqf3a4i-uontjo6&jl2tg6%+#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 
 # set up custom user model
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
